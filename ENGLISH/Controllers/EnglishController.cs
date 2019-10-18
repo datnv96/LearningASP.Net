384528c890779cc3f0ca022ee2f87943cdb551d3
DatNV <datnv@kaopiz.com>
2019-10-18 14:54:26 +0700

    update English

---
+++ ENGLISH/Controllers/EnglishController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ENGLISH.Controllers
+{
+    public class EnglishController : Controller
+    {
+        public string Wellcome(string name, int ID = 1)
+        {
+            return System.Text.Encodings.Web.HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
